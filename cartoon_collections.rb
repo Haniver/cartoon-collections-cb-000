6ae2635455aeb6d8d4047b0279f5bb65febe586b
Lucio Ernesto Gutierrez Munch <luciogm@gmail.com>
2018-04-04 22:35:33 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,8 +9,8 @@ def summon_captain_planet(planets)
   planets.map {|planet| planet.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? { |call| call.length > 4 }
 end
 
 def find_the_cheese# code an argument here
