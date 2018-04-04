5a583c8ef63413f69135f60a825294be0eb3a694
Lucio Ernesto Gutierrez Munch <luciogm@gmail.com>
2018-04-04 22:45:24 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,5 +18,3 @@ def find_the_cheese(strings)
   cheese_types = ["cheddar", "gouda", "camembert"]
   strings.find{|i| i == cheese_types[0] || i == cheese_types[1] || i == cheese_types[2]}
 end
-
-binding.pry
